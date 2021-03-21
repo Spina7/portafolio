cca27dcffbd433df86ff914b5786e828b8c3e48f
Sergio Piña <alonzopina7@gmail.com>
2021-03-21 15:49:28 -0600

    final3

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,30 @@ function scrollActive() {
     }
   });
 }
+
+//Form
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Mensaje Enviado!";
+      status.classList.add("success");
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! Hubo un error al enviar el mensaje";
+      status.classList.add("error");
+    });
+}
+form.addEventListener("submit", handleSubmit);
